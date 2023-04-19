9f98901d798a2b90251e8c55df32ae0f7182af6d
sakshi009005 <sakshi.gupta@outlook.com>
2023-04-19 14:56:40 +0530

    grodiff app init

---
+++ src/main/java/com/grocery/diff/GrodiffApplication.java
@@ -0,0 +1,12 @@
+package com.grocery.diff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrodiffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrodiffApplication.class, args);
+	}
+}
---
+++ src/test/java/com/grocery/diff/GrodiffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grocery.diff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrodiffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
