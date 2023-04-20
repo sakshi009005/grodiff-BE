177d624ba7d180e3edc8582d6fd8d63717fcccb8
sakshi009005 <sakshi.gupta@outlook.com>
2023-04-20 18:47:18 +0530

    intial api blinkit

--- src/main/java/com/grocery/diff/GrodiffApplication.java
+++ src/main/java/com/grocery/diff/GrodiffApplication.java
@@ -2,11 +2,19 @@ package com.grocery.diff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class GrodiffApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GrodiffApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 }
